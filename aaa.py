ee0a5da7265e10dd33bad0859baf95cd0c1ffc25
bubu3255 <dashhem@gmail.com>
2026-05-07 19:04:33 +0530

    Create aaa.py

---
+++ aaa.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+BOT_TOKEN = "8764471009:AAE5kTp_zNTAAvMwlseOTqs8KvNrjGrutrI"
+CHAT_ID = "-1003800263291"
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json
+
+    message = data.get("message", "TradingView Alert")
+
+    url = f"https://api.telegram.org/bot8764471009:AAE5kTp_zNTAAvMwlseOTqs8KvNrjGrutrI/sendMessage"
+
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+
+    requests.post(url, json=payload)
+
+    return "ok"
+
+@app.route('/')
+def home():
+    return "TrendStrike Bot Running"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
